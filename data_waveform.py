2298e5acc9deb486e7d7c47c67393f779046f8a2
Yang Taotao <74541908+Yang-Taotao@users.noreply.github.com>
2023-06-16 00:46:35 +0100

    Data module update

--- data_waveform.py
+++ data_waveform.py
@@ -7,6 +7,16 @@ Created on Thu Jun 14 2023
 """
 
 # %% Section 0 - Library import
+# JAX related import
+import jax.numpy as jnp
+from jax import grad, vmap
+# RIPPLE related import
+from ripple.waveform import IMPRhenomD
+from ripple import ms_to_Mc_eta
+# Other import
+from functools import partial
+from math import pi
+import matplotlib.pyplot as plt
 
 # %% Section 1 - 
 
