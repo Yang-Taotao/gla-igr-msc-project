70ad703c649cd3026345d707fc2da7b79e9514d2
Yang Taotao <yangtaotao0524@gmail.com>
2023-06-20 12:43:33 +0100

    Waveform generator refactor

--- data_waveform.py
+++ data_waveform.py
@@ -7,72 +7,64 @@ Created on Thu Jun 14 2023
 """
 
 # %% Section 0 - Library import
-# JAX related import
+# Module - jax related import
 import jax.numpy as jnp
-from jax import grad, vmap
-# RIPPLE related import
+# Module - ripple related import
 from ripple.waveforms import IMRPhenomXAS
 from ripple import ms_to_Mc_eta
-# Other import
-from functools import partial
-from math import pi
+# Module - other import
 import matplotlib.pyplot as plt
 import scienceplots
 
-# %% Section 1 - Waveform param repo
-theta = (
-    (36.0, 29.0),   # Mass: in unit of solar masses
-    (0, 0),         # Spin: no spin
-    (0.0, 0.0),     # Time of coalescence in seconds, and Time of coalescence
-    440,            # Distance to source in Mpc
-    (0.0, 0.2),     # Inclination angle, and Polarization angle
-)
+# %% Section 0 - Plotter style customization
+plt.style.use(["science", "notebook", "grid"])
 
-# Invoke ms_to_Mc_eta
-Mc, eta = ms_to_Mc_eta(jnp.array([theta[0][0], theta[0][1]]))
+# %% Section 1 - Waveform generation function
+def data_ripple(theta):
+    # Waveform generator
+    # Invoke ms_to_Mc_eta method
+    theta_mc, theta_eta = ms_to_Mc_eta(jnp.array([theta[0][0], theta[0][1]]))
+    # Intermidiate calculations - ripple
+    theta_ripple = jnp.array(
+        [
+            theta_mc,             
+            theta_eta,            
+            theta[1][0],    
+            theta[1][1],    
+            theta[3],       
+            theta[2][0],    
+            theta[2][1],    
+            theta[4][0], 
+            theta[4][1],
+        ]
+    )
+    # Signal freq and ref freq
+    freq_sig, freq_ref = (
+        jnp.arange(theta[5], theta[6], theta[7]),
+        theta[5],
+    )
+    # Strain signal generator
+    strain_plus, strain_cros = IMRPhenomXAS.gen_IMRPhenomXAS_polar(freq_sig, theta_ripple, freq_ref)
+    
+    # Plotter
+    # Plot init
+    plt.figure(figsize=(15, 5))
+    # Plotting
+    plt.plot(freq_sig, strain_plus.real, label="h_+ ripple", alpha=0.3)
+    plt.plot(freq_sig, strain_cros.imag, label="h_x ripple", alpha=0.3)
+    # Plot customizer
+    plt.legend()
+    plt.xlabel("Frequency")
+    plt.ylabel("Signal Strain")
+    plt.savefig("./media/fig_01_ripple_waveform.png")
+    plt.close()
 
-# %% Section 2 - Ripple
-# Ripple
-theta_ripple = jnp.array(
-    [
-        Mc,             
-        eta,            
-        theta[1][0],    
-        theta[1][1],    
-        theta[3],       
-        theta[2][0],    
-        theta[2][1],    
-        theta[4][0], 
-        theta[4][1],
-    ]
-)
-
-# %% Section 3 - Additional param
-# Freq grid generator
-theta_grid = (
-    24,         # Grid length
-    512,        # Grid depth
-    0.01,       # Grid step
-)
-
-# Signal freq and ref
-f_sig, f_ref = (
-    jnp.arange(theta_grid[0], theta_grid[1], theta_grid[2]),
-    theta_grid[0],
-)
-
-# %% Section 4 - Waveform generator
-hp_ripple, hc_ripple = IMRPhenomXAS.gen_IMRPhenomXAS_polar(f_sig, theta_ripple, f_ref)
-
-# %% Section 5 - Waveform plotter
-# Plot init
-plt.figure(figsize=(15, 5))
-# Plotting
-plt.plot(f_sig, hp_ripple.real, label="h+ ripple", alpha=0.3)
-plt.plot(f_sig, hc_ripple.imag, label="hx ripple", alpha=0.3)
-# Plot customizer
-plt.legend()
-plt.xlabel("Freq")
-plt.ylabel("Strain")
-plt.savefig("./media/fig_01_ripple_waveform.png")
-plt.close()
+    # Function result gather
+    result = (
+        freq_sig,
+        freq_ref,
+        strain_plus,
+        strain_cros,
+    )
+    # Function return
+    return result
--- main.py
+++ main.py
@@ -7,3 +7,30 @@ Created on Thu Jun 14 2023
 """
 
 # %% Section 0 - Library import
+from data_waveform import data_ripple
+
+# %% Section 1 - Waveform generator
+# Define waveform generation parameters
+theta_waveform_gen = (
+    (36.0, 29.0),   # Mass: in units of solar masses
+    (0, 0),         # Spin: no spin
+    (0.0, 0.0),     # Time of coalescence in seconds, and Time of coalescence
+    440,            # Distance to source in Mpc
+    (0.0, 0.2),     # Inclination angle, and Polarization angle
+)
+# Define waveform plotter parameters
+theta_waveform_plot = (
+    24,         # 
+    512,        # 
+    0.01,       # 
+)
+# Combined waveform parameters
+theta_waveform = theta_waveform_gen + theta_waveform_plot
+# Call ripple waveform generator and plotter
+data_ripple(theta_waveform)
+
+# %% Section 2 - Fisher matrix calculator
+
+
+# %% Section 3 - Probability density
+
