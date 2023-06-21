1706bdf3a39f73c0e4a079a294617452260f4552
Yang Taotao <yangtaotao0524@gmail.com>
2023-06-21 14:39:07 +0100

    Waveform function refactor

--- data_waveform.py
+++ data_waveform.py
@@ -8,6 +8,7 @@ Created on Thu Jun 14 2023
 
 # %% Section 0 - Library import
 # Module - jax related import
+import jax
 import jax.numpy as jnp
 # Module - ripple related import
 from ripple.waveforms import IMRPhenomXAS
@@ -21,29 +22,39 @@ plt.style.use(["science", "notebook", "grid"])
 
 # %% Section 1 - Waveform generation function
 
+# Dummy func
+# def waveform(chirp_mass, mass_ratio, a1, a2, inclination, psi, distance, tc, phi):
+#     # Build the theta tuple for ripple
+#     # Build the frequency vector for ripple
+#     f_ref = f_min
+#     hp, hc = ripple.IMRPhenon(f_sig, theta, f_ref)
+#     return hp, hc
+# jax.grad(waveform, 0) # derivative w.r.t Mc
+# jax.grad(waveform, 1) # deriv w.r.t mass_ratio
 
-def data_ripple(theta):
-    # Waveform generator
-    # Invoke ms_to_Mc_eta method
-    theta_mc, theta_eta = ms_to_Mc_eta(jnp.array([theta[0][0], theta[0][1]]))
+def data_ripple(theta, config):
+    # Invoke ms_to_Mc_eta method: chirp mass, eta
+    mc, eta = ms_to_Mc_eta(jnp.array([theta[0], theta[1]]))
     # Intermidiate calculations - ripple
     theta_ripple = jnp.array(
         [
-            theta_mc,
-            theta_eta,
-            theta[1][0],
-            theta[1][1],
-            theta[3],
-            theta[2][0],
-            theta[2][1],
-            theta[4][0],
-            theta[4][1],
+            mc,         # Mass - chirp
+            eta,        # eta
+            theta[2],   # Spin - 1
+            theta[3],   # Spin - 2
+            theta[4],   # Distance - Mpc
+            theta[5],   # Coalescence - time
+            theta[6],   # Coalescence - phase
+            theta[7],   # Angle - inclination
+            theta[8],   # Angle - ploarization
         ]
     )
     # Signal freq and ref freq
     f_sig, f_ref = (
-        jnp.arange(theta[5], theta[6], theta[7]),
-        theta[5],
+        # Construct sig freq
+        jnp.arange(config[0], config[1], config[2]),
+        # Set refrence sig freq as min freq
+        config[0],
     )
     # Strain signal generator
     h_plus, h_cros = IMRPhenomXAS.gen_IMRPhenomXAS_polar(
@@ -54,11 +65,23 @@ def data_ripple(theta):
 
     # INSERT JIT STUFF
 
-    # Plotter
+    # Function result gather
+    result = (
+        f_sig,
+        h_plus,
+        h_cros,
+    )
+    # Function return
+    return result, theta_ripple
+
+# %% Section 2 - Ripple waveform plotter
+def plot_ripple(theta):
+    # Local variable repo
+    f_sig, h_plus, h_cros = theta[0]
     # Plot init
     plt.figure(figsize=(15, 5))
     # Plotting for alingned spin sys
-    # Modulation for angular momentum precession
+    # No modulation for angular momentum precession
     plt.plot(
         f_sig, 
         h_plus.real, 
@@ -79,17 +102,27 @@ def data_ripple(theta):
     plt.close()
 
     # Secondary plot - Inverse FFT - preliminary
-    h_plus_irfft = jnp.fft.irfft(h_plus) #inverse real fft, need to start from zero freqs
-    plt.plot(jnp.arange(0, 1/f_sig[1], 1/(2*f_sig[-1])), jnp.roll(h_plus_irfft, -50))
-    plt.savefig("./media/tfft.png")
-    plt.close()
+    # h_plus_irfft = jnp.fft.irfft(h_plus) #inverse real fft, need to start from zero freqs
+    # plt.plot(jnp.arange(0, 1/f_sig[1], 1/(2*f_sig[-1])), jnp.roll(h_plus_irfft, -50))
+    # plt.savefig("./media/tfft.png")
+    # plt.close()
 
-    # Function result gather
-    result = (
-        f_sig,
-        f_ref,
-        h_plus,
-        h_cros,
-    )
-    # Function return
+# %% Section 3 - Derivative calculator
+def data_grad(data_ripple, theta_ripple):
+    # Grad func assignment
+    grad_func = jax.grad(data_ripple)
+    # Build results
+    result = jax.vmap(grad_func)(theta_ripple)
+    # Func return
+    return result
+
+# %% Section 4 - Simple function calls
+def func_ripple(theta, config):
+    # Call all other functions
+    output_wave, output_theta = data_ripple(theta, config)
+    output_grad = data_grad(data_ripple(theta, config), output_theta)
+    plot_ripple(output_wave)
+    # Results tally
+    result = output_wave, output_grad
+    # Func return
     return result
