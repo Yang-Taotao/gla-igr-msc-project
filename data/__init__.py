eacb67d6546f84898ff61bebfd7ee96a3496ef14
Yang Taotao <yangtaotao0524@gmail.com>
2023-07-21 16:00:06 +0100

    Packaging refactor

--- data/__init__.py
+++ data/__init__.py
@@ -5,21 +5,6 @@ Created on Thu Jul 21 2023
 
 @author: Yang-Taotao
 """
-from .gw_ripple import (
-    waveform,
-    freq_build,
-    waveform_plus,
-    waveform_cros,
-    grad_vmap,
-)
-from .gw_fisher import (
-    bilby_psd,
-    mat,
-    sqrtdet,
-)
-from .gw_plotter import (
-    ripple_waveform_plot,
-    ripple_grad_plot_idx,
-    bilby_plot,
-    fim_plot,
-)
+from .gw_ripple import *
+from .gw_fisher import *
+from .gw_plotter import *
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Created on Thu Jul 11 2023
 """
 # %%
 # Section 0 - Library import
-import data
+from data import gw_ripple, gw_fisher, gw_plotter
 
 # %%
 # Section 1.a -  Define GW data theta
@@ -19,37 +19,37 @@ data_freq = (24.0, 512.0, 0.5)
 # %%
 # Section 1.b -  Generate GW data
 # t ~ 37.6s
-data_hp, data_hc = data.waveform(data_theta)
-f_sig, _ = data.freq_build(data_freq)
+data_hp, data_hc = gw_ripple.waveform(data_theta)
+f_sig, _ = gw_ripple.freq_build(data_freq)
 
 # %%
 # Section 1.c -  Plot GW data - GW170817
 data_theta_plot = data_hp, data_hc, f_sig
-data.ripple_waveform_plot(data_theta_plot)
+gw_plotter.ripple_waveform_plot(data_theta_plot)
 
 # %%
 # Section 2.a -  Generate mapped grad data
 # t ~ 1m51.8s
-data_hp_grad = data.grad_vmap(data.waveform_plus, data_theta)
-data_hc_grad = data.grad_vmap(data.waveform_cros, data_theta)
+data_hp_grad = gw_ripple.grad_vmap(gw_ripple.waveform_plus, data_theta)
+data_hc_grad = gw_ripple.grad_vmap(gw_ripple.waveform_cros, data_theta)
 
 # %%
 # Section 2.b -  Plot GW data - grad wrt data_idx
 data_idx = 0, 1
 data_theta_grad_plot = data_hp_grad, data_hc_grad, f_sig
-data.ripple_grad_plot_idx(data_theta_grad_plot, *data_idx)
+gw_plotter.ripple_grad_plot_idx(data_theta_grad_plot, *data_idx)
 
 # %%
 # Section 3.a - FIM psd plot
-data_theta_bilby = data_freq, data.bilby_psd(data_freq)
-data.bilby_plot(data_theta_bilby)
+data_theta_bilby = data_freq, gw_fisher.bilby_psd(data_freq)
+gw_plotter.bilby_plot(data_theta_bilby)
 
 # %%
 # Section 3.b - FIM and sqrt of det of matrix 
 data_idx_test = tuple(range(9))
-data_fim = data.mat(data_hp_grad, data_idx_test)
-data_fim_sqrt_det = data.sqrtdet(data_hp_grad, data_idx)
+data_fim = gw_fisher.mat(data_hp_grad, data_idx_test)
+data_fim_sqrt_det = gw_fisher.sqrtdet(data_hp_grad, data_idx)
 
 # %%
 # Section 3.c - FIM plot
-data.fim_plot(data_fim)
+gw_plotter.fim_plot(data_fim)
