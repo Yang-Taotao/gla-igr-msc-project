a0d9de47a6a660cba90709fb213ac75871a19784
John Veitch <john.veitch@ligo.org>
2023-08-02 20:21:39 +0100

    Add code to compute projected metric

--- data/gw_ripple.py
+++ data/gw_ripple.py
@@ -7,6 +7,8 @@ Created on Thu Jul 11 2023
 # %%
 # Library import
 import os
+# XLA GPU resource setup
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
 # Package - jax
 import jax
 import jax.numpy as jnp
@@ -14,17 +16,16 @@ import jax.numpy as jnp
 from ripple.waveforms import IMRPhenomXAS
 # Custom config import
 from data.gw_config import f_sig, f_ref, theta_base, f_psd, f_diff
-# XLA GPU resource setup
-os.environ['XLA_PYTHON_CLIENT_PREALLOCATE'] = 'false'
+
 
 # %%
 # Ripple - waveform generator
-
+@jax.jit
 def innerprod(a,b):
     """
     Noise weighted inner product between vectors a and b
     """
-    numerator = jnp.real(a.conj()*b)
+    numerator = jnp.abs(a.conj()*b)
     #assert jnp.isnan(numerator).sum() == 0
     integrand = numerator / f_psd
     #assert jnp.isnan(integrand).sum() == 0
@@ -32,58 +33,33 @@ def innerprod(a,b):
     #assert not jnp.isnan(result)
     return result
 
+
 @jax.jit
-def waveform(theta: jnp.ndarray):
-    # Generate and return strain waveform - plus, cross
-    return IMRPhenomXAS.gen_IMRPhenomXAS_polar(
-        f_sig, theta, f_ref)
-
-
-def waveform_plus_mceta(mceta: jnp.ndarray, freq: jnp.ndarray):
-    # Generate strain waveform - plus, cross
-    theta = theta_base.at[0:2].set(mceta)
-    h_plus, _ = IMRPhenomXAS.gen_IMRPhenomXAS_polar(
-        jnp.array([freq]), theta, f_ref)
+def waveform_plus_restricted(params: jnp.ndarray, freq: jnp.ndarray):
+    '''
+    Function to return waveform where params are:
+    [Mc, eta, t_c, phi_c]
+    '''
+    theta = theta_base.at[0:2].set(params[0:2]).at[5:7].set(params[2:4])
+    h_plus, _ = IMRPhenomXAS.gen_IMRPhenomXAS_polar(freq, theta, f_ref)
     # Return ripple waveform tuple
     return h_plus
 
-def waveform_plus_normed(mceta: jnp.ndarray, freq: jnp.ndarray):
-    wf = waveform_plus_mceta(mceta, freq)
+@jax.jit
+def waveform_plus_normed(params: jnp.ndarray, freq: jnp.ndarray):
+    wf = waveform_plus_restricted(params, freq)
     N = innerprod(wf, wf)
-    return wf[0]/jnp.complex128(jnp.sqrt(N))
+    return wf/jnp.sqrt(N)
 
-def waveform_cros(theta: jnp.ndarray, freq: jnp.ndarray):
-    # Generate strain waveform - plus, cross
-    _, h_cros = IMRPhenomXAS.gen_IMRPhenomXAS_polar(
-        jnp.array([freq]), theta, f_ref)
-    # Return ripple waveform tuple
-    return h_cros.imag[0]
 
 # %%
 # Ripple - gradient calculator
 
 @jax.jit
-def gradient_plus_mceta(theta: jnp.ndarray):
+def gradient_plus(theta: jnp.ndarray):
     # Calculate gradient and return
-    mceta = jnp.array(theta, dtype=jnp.complex128)
-    return jax.vmap(jax.grad(waveform_plus_normed, holomorphic=True),in_axes=(None, 0))(mceta, f_sig)
+    params = jnp.array(theta, dtype=jnp.complex128)
+    #params = theta
+    return jax.vmap(jax.grad(waveform_plus_normed, holomorphic=True),
+                    in_axes=(None, 0))(params, f_sig)
 
-
-def gradient_cros(theta: jnp.ndarray):
-    # Calculate gradient and return
-    return jax.vmap(jax.grad(waveform_cros), in_axes=(None, 0))(theta, f_sig)
-
-# %%
-# Ripple - gradient calculator with matrix support
-
-
-@jax.jit
-def grad_plus(theta: jnp.ndarray):
-    # Return matrix form of gradients
-    return jax.vmap(gradient_plus)(theta)
-
-
-@jax.jit
-def grad_cros(theta: jnp.ndarray):
-    # Return matrix form of gradients
-    return jax.vmap(gradient_cros)(theta)
--- test.py
+++ test.py
@@ -1,30 +1,49 @@
-import jax.numpy as jnp
+import os
+os.environ['XLA_PYTHON_CLIENT_PREALLOCATE']='false'
 from data import gw_ripple, gw_fisher, gw_plotter
 from data.gw_config import f_sig, f_psd, mc_repo, mr_repo, theta_repo
-from data.gw_fisher import fim
-from data.gw_ripple import gradient_plus_mceta, waveform_plus_mceta, waveform_plus_normed
+from data.gw_fisher import fim, projected_fim
+from data.gw_ripple import gradient_plus, waveform_plus_restricted, waveform_plus_normed
+import jax.numpy as jnp
 
 from matplotlib import pyplot as plt
+import itertools as it
+from tqdm import tqdm
 
 mc = 20
 eta = 0.2
 mceta = jnp.array([mc, eta])
+fisher_params = jnp.array([mc, eta, 0.0, 0.0])
 
-#wf = waveform_plus_normed(mceta)
-#print(wf)
-g = gradient_plus_mceta(mceta)
-print(g)
-plt.plot(g[:,0].real)
-plt.plot(g[:,0].imag)
-plt.show()
-G = fim(mceta)
+wf = waveform_plus_restricted(fisher_params, f_sig)
+print(wf.shape)
+g = gradient_plus(fisher_params)
+print(g.shape)
+# print(g)
+# plt.plot(g[:,0].real)
+# plt.plot(g[:,0].imag)
+# plt.show()
+G = projected_fim(fisher_params)
+print("Projected FIM: ",G)
+
+mcs = jnp.arange(3,10,0.1)
+etas = jnp.arange(0.05,0.25,0.005)
+
+gridpoints = it.product(mcs,etas)
 
 density = []
-mcs = jnp.arange(5,20,0.1)
-for mc in mcs:
-    mceta = jnp.array([mc, eta])
-    G = fim(mceta)
-    detg = jnp.sqrt(jnp.linalg.det(G))
+for x in tqdm(gridpoints, total=len(mcs)*len(etas)):
+    mceta = jnp.array([x[0],x[1],0.0,0.0])
+    try:
+        G = projected_fim(mceta)
+        detg = jnp.sqrt(jnp.linalg.det(G))
+    except AssertionError:
+        detg = jnp.nan
     density.append(detg)
 
-plt.plot(mcs, density)
+output = jnp.array(density).reshape([len(mcs),len(etas)])
+plt.contourf(mcs, etas, output.T)
+plt.colorbar().ax.set_ylabel('Template bank density')
+plt.xlabel('Chirp mass ($M_\odot$)')
+plt.ylabel('Symmetric mass ratio $\eta$')
+plt.savefig('projected_metric.png')
