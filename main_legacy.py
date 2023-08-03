703480366b73a0510e3591caf07b4555a92ba636
Yang Taotao <yangtaotao0524@gmail.com>
2023-08-03 13:11:57 +0100

    Main script update

--- main_legacy.py
+++ main_legacy.py
@@ -22,8 +22,8 @@ mock_theta = jnp.tile(mock_waveform_theta, (theta_repo.shape[0], 1))
 # Section 1.b -  Mock GW data generate - waveform, grad
 # t ~ 3min58.8s - first compile
 mock_hp, mock_hc = gw_ripple.waveform(mock_waveform_theta)
-mock_hp_grad, mock_hc_grad = gw_ripple.grad_plus(
-    mock_theta)[0], gw_ripple.grad_cros(mock_theta)[0]
+mock_hp_grad = gw_ripple.grad_plus(mock_theta)[0]
+mock_hc_grad = gw_ripple.grad_cros(mock_theta)[0]
 mock_idx = jnp.array([0, 1])
 
 # %%
@@ -35,19 +35,13 @@ mock_fim_sqrtdet = gw_fisher.sqrtdet_fim(mock_hp_grad, mock_idx)
 # %%
 # Section 2.a - GW data generation - mc, mr - grad
 # t ~ 0.4s
-data_hp_grad_repo, data_hc_grad_repo = gw_ripple.grad_plus(
-    theta_repo), gw_ripple.grad_cros(theta_repo)
+data_hp_grad_repo = gw_ripple.grad_plus(theta_repo)
+data_hc_grad_repo = gw_ripple.grad_cros(theta_repo)
 
 # %%
 # Section 2.b - GW data generation - mc, mr - sqrt.det.fim
-data_fim_hp_repo = jnp.array([
-    [gw_fisher.sqrtdet_fim(data_hp_grad_repo[i, :, :], mock_idx)]
-    for i in range(data_hp_grad_repo.shape[0])
-])
-data_fim_hc_repo = jnp.array([
-    [gw_fisher.sqrtdet_fim(data_hc_grad_repo[i, :, :], mock_idx)]
-    for i in range(data_hc_grad_repo.shape[0])
-])
+data_fim_hp_repo = gw_fisher.sqrtdet_fim(data_hp_grad_repo, mock_idx)
+data_fim_hc_repo = gw_fisher.sqrtdet_fim(data_hc_grad_repo, mock_idx)
 
 # %%
 # Section 3 - Plot generation
@@ -59,5 +53,5 @@ gw_plotter.fim_plot(mock_fim)
 gw_plotter.bilby_plot(f_sig, f_psd)
 # Generated fim and contour
 gw_plotter.fim_param_2d(data_fim_hp_repo, data_fim_hc_repo, mc_repo, mr_repo)
-# gw_plotter.fim_param_3d(data_fim_hp_repo, data_fim_hc_repo, mc_repo, mr_repo)
+gw_plotter.fim_param_3d(data_fim_hp_repo, data_fim_hc_repo, mc_repo, mr_repo)
 gw_plotter.fim_contour_mc_mr(data_fim_hp_repo, data_fim_hc_repo, mc_repo, mr_repo)
